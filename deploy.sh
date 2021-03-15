606ab652220f1944261d73f2f03b69c1b5395822
xuqisheng <Xqs154473>
2021-03-15 20:04:32 +0800

    deploy.sh modified

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:xuqisheng1/xuqisheng1.github.io.git master:gh-pages
+git push -f git@github.com:xuqisheng1/blog.git master:gh-pages
 
 cd -
